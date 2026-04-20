860c600689ff4ccfc38be4f0a9690dd809eb4828
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2026-04-20 21:19:01 +0600

    added kubernetes on-premise setup code

--- scripts/Kubernetes/run.sh
+++ scripts/Kubernetes/run.sh
@@ -31,3 +31,22 @@ kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 # nginx ingress
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.7.0/deploy/static/provider/baremetal/deploy.yaml
 
+# for kubernetes dashboard
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
+
+kubectl create serviceaccount dashboard-admin -n default
+kubectl create clusterrolebinding dashboard-admin-binding --clusterrole=cluster-admin --serviceaccount=default:dashboard-admin
+
+kubectl create token dashboard-admin -n default
+
+apiVersion: v1
+kind: Secret
+metadata:
+  name: dashboard-admin-token
+  annotations:
+    kubernetes.io/service-account.name: dashboard-admin
+type: kubernetes.io/service-account-token       
+
+
+kubectl get secret dashboard-admin-token -n default -o jsonpath="{.data.token}" | base64 --decode
+eyJhbGciOiJSUzI1NiIsImtpZCI6IjhvVENfWmY1Vzl5NU9oMllFUFlFVjZmMHN2azJrZUVVSkM3UUlvbFNOQ3MifQ.eyJhdWQiOlsiaHR0cHM6Ly9rdWJlcm5ldGVzLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWwiXSwiZXhwIjoxNzc2Njk3MTAyLCJpYXQiOjE3NzY2OTM1MDIsImlzcyI6Imh0dHBzOi8va3ViZXJuZXRlcy5kZWZhdWx0LnN2Yy5jbHVzdGVyLmxvY2FsIiwianRpIjoiN2Y3YmY3NWQtYmFjZi00YTVlLWFhZmUtZjU2MmJhMmRhZDZkIiwia3ViZXJuZXRlcy5pbyI6eyJuYW1lc3BhY2UiOiJkZWZhdWx0Iiwic2VydmljZWFjY291bnQiOnsibmFtZSI6ImRhc2hib2FyZC1hZG1pbiIsInVpZCI6ImUxYmEzMWUxLTMyMzctNGVjMy1iNDY4LWM2ZThkN2UzMTJiYiJ9fSwibmJmIjoxNzc2NjkzNTAyLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6ZGVmYXVsdDpkYXNoYm9hcmQtYWRtaW4ifQ.oSYUVEegzUnggCFmXvPqFcgYa16Rq5kVOcZ05jypn2YrUo0CfznrtP6302MN8254MPi3n34GVl4cdC0SJAUhCct8KcpP0jTVySXPoiLAzuHyiTZ8y4mnOg1RVrZEuNx58UG9D1kK2u35U_ShVjHaorxrTpm6Li1e-NlmAz5-3MkNMhCpRiOHWu1i8NbY9v4lOU-ZBX4AdTRMGbXyR0vkbULKYjlmZIm1PDI_tFiJ8AmSb9geuP2mJWDzfqq0W7hImtvBQML4siQuYUEEhgK4902AyBAiApxneTKoFRPDtO4dQ5-jVtcPGhJJQvQMa5ggYQt36EjM-0qQloKkQPvFWQ
